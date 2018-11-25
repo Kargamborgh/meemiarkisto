e5b0e63563323280a8f266fbfc5d227147c066a9
Kargamborgh <niilo.korkiakoski@gmail.com>
2018-11-25 14:50:08 +0200

    week 3 part 4: heroku configuration + db setup

--- application/__init__.py
+++ application/__init__.py
@@ -2,10 +2,17 @@
 from flask import Flask
 app = Flask(__name__)
 
-# database stuff
+# local database stuff
 from flask_sqlalchemy import SQLAlchemy
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///memes.db"
-app.config["SQLALCHEMY_ECHO"] = True
+
+import os
+
+# database switch depending on heroku/local
+if os.environ.get("HEROKU"):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///memes.db"
+    app.config["SQLALCHEMY_ECHO"] = True
 
 db = SQLAlchemy(app)
 
@@ -36,4 +43,7 @@ def load_user(user_id):
     return User.query.get(user_id)
 
 # create database tables if needed
-db.create_all()
+try:
+    db.create_all()
+except:
+    pass
