2fad14b423c7811425a7ff25d4174221b007f2f4
Kargamborgh <niilo.korkiakoski@gmail.com>
2018-11-25 14:13:37 +0200

    week 3 part 2.5: login_required

--- application/memes/views.py
+++ application/memes/views.py
@@ -1,5 +1,6 @@
 from application import app, db
 from flask import redirect, render_template, request, url_for
+from flask_login import login_required
 from application.memes.models import Meme
 from application.memes.forms import MemeForm
 
@@ -8,10 +9,12 @@ def memes_index():
     return render_template("memes/list.html", memes = Meme.query.all())
 
 @app.route("/memes/new/")
+@login_required
 def memes_form():
     return render_template("memes/new.html", form = MemeForm())
 
 @app.route("/memes/<meme_id>/", methods=["POST"])
+@login_required
 def memes_increase_score(meme_id):
 
     m = Meme.query.get(meme_id)
@@ -20,26 +23,23 @@ def memes_increase_score(meme_id):
 
     return redirect(url_for("memes_index"))
 
-# Implement this next week
+def memes_decrease_score(meme_id):
 
-#@app.route("/memes/<meme_id>/", methods=["POST"])
-#def memes_decrease_score(meme_id):
+   m = Meme.query.get(meme_id)
+   m.points = m.points-1
+   db.session().commit()
 
-#    m = Meme.query.get(meme_id)
-#    m.points =-1
-#    db.session().commit()
-
-#    return redirect(url_for("memes_index"))
+   return redirect(url_for("memes_index"))
 
 @app.route("/memes/", methods=["POST"])
+@login_required
 def memes_create():
     form = MemeForm(request.form) ## right now Meme consists of an image file and a title
 
     if not form.validate():
         return render_template("memes/new.html", form = form)
 
-    m = Meme(form.image.data)
-    m.title = form.title.data
+    m = Meme(title=form.title.data, points=0)
 
     db.session().add(m)
     db.session().commit()
