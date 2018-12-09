6e0cbf56ad9b07fd61113998556a06f276813e1d
Kargamborgh <niilo.korkiakoski@gmail.com>
2018-12-09 20:28:26 +0200

    some bootstrap, navbar, slightly prettier view page

--- application/memes/models.py
+++ application/memes/models.py
@@ -3,9 +3,8 @@ from application.models import Base
 
 from flask_uploads import configure_uploads
 
-# A meme consist of an image, a title, points, filename (when uploading),
+# A meme consist of an image, a title, points, filename,
 # id of user who uploaded the meme, 0-* comments
-# A meme image will later be served from '/images' to correspond to a meme entry
 
 class Meme(Base):
 
@@ -17,8 +16,6 @@ class Meme(Base):
     filename = db.Column(db.String(144), nullable=True)
     url = db.Column(db.String(144), nullable=True)
     account_id = db.Column(db.Integer, db.ForeignKey('account.id'), nullable=False)
-    # comment table breaks things because of emptiness and relationships, commented out for now
-    # comment_id = db.relationship("Comment", backref="comment", lazy=True)
 
 
     def __init__(self, title, points, filename, url, account_id):
