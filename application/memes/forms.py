b6fa09a69a40fc3b8eb1e8446b2d2850e12e0084
Kargamborgh <niilo.korkiakoski@gmail.com>
2018-12-14 03:35:21 +0200

    AWS s3 file storage works now

--- application/memes/forms.py
+++ application/memes/forms.py
@@ -2,14 +2,13 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, validators
 from flask_wtf.file import FileField, FileAllowed, FileRequired
 from wtforms.validators import DataRequired
-from application import images
 
 # user interaction in meme submission is limited to file(image) and a title
 
 class MemeForm(FlaskForm):
 
     title = StringField('Image description', [validators.Length(min=3)])
-    image = FileField('Image', validators=[FileRequired(), FileAllowed(images,)])
+    image = FileField('Image', validators=[FileRequired()])
     submit = SubmitField('Submit new meme')
 
     class Meta:
